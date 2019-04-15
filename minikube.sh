50b28859dd8a85039d4b7680c09769fbc6faf669
sikor <itmaciej.sikora@gmail.com>
2019-04-16 00:08:22 +0200

    jenkins-file

---
+++ minikube.sh
@@ -0,0 +1,52 @@
+######################
+# Create The Cluster #
+######################
+
+# Tested with minikube v0.30
+
+minikube start \
+    --vm-driver virtualbox \
+    --cpus 2 \
+    --memory 10240
+
+###############################
+# Install Ingress and Storage #
+###############################
+
+minikube addons enable ingress
+
+minikube addons enable storage-provisioner
+
+minikube addons enable default-storageclass
+
+##################
+# Install Tiller #
+##################
+
+kubectl create \
+    -f https://raw.githubusercontent.com/vfarcic/k8s-specs/master/helm/tiller-rbac.yml \
+    --record --save-config
+
+helm init --service-account tiller
+
+kubectl -n kube-system \
+    rollout status deploy tiller-deploy
+
+##################
+# Metrics Server #
+##################
+
+minikube addons enable metrics-server
+
+kubectl -n kube-system rollout status deployment metrics-server
+
+##################
+# Get Cluster IP #
+##################
+
+export LB_IP=$(minikube ip)
+    
+#######################
+# Destroy the cluster #
+#######################
+
